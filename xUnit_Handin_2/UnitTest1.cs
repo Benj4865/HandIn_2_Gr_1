3adb26e8252ee2dff9d5d04e2dcfd306cad784d8
Nikolaj Isager Kruuse <nik@ruc.dk>
2024-11-01 12:26:28 +0100

    Push test for Nikolaj

--- xUnit_Handin_2/UnitTest1.cs
+++ xUnit_Handin_2/UnitTest1.cs
@@ -5,8 +5,19 @@ namespace xUnit_Handin_2
         [Fact]
         public void Test1()
         {
-            
+
 
         }
-    }
+
+
+        [Fact]
+        public void Get_actor_ID_as_String()
+        {
+            //GetActor unit test Nikolaj. 
+            var Actor = new Actor();
+            Assert.Equal(0,)
+
+
+        }
+}
 }
