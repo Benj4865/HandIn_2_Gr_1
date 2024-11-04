6eadbc29d2b248ce123a5597b956ffad7db5fdce
Benj4865 <bena380dk@gmail.com>
2024-11-04 10:44:05 +0100

    Removed tests changes

--- HandIn_2_Gr_1/DataServiceTitle.cs
+++ HandIn_2_Gr_1/DataServiceTitle.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// Test change
+
 namespace HandIn_2_Gr_1
 {
     internal class DataServiceTitle
