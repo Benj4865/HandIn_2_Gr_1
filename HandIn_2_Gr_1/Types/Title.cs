4b36a331c1f69b3a908c2e1953e17319ad77cb66
Mikkel <Mikkel@LAPTOP-D946K30V>
2024-11-04 12:28:35 +0100

    Added Averagerating and Numberofvotes to title.cs

--- HandIn_2_Gr_1/Types/Title.cs
+++ HandIn_2_Gr_1/Types/Title.cs
@@ -26,6 +26,9 @@ namespace HandIn_2_Gr_1.Types
 
         public IList<Genre>? Genre { get; set; }
 
+        public double Averagerating { get; set; }
+         
+        public int Numberofvotes { get; set; }
 
         //Below is a custom attribute, indicating wether or not the title is a in a series or is a standalone movie
         public bool IsEpisode { get; set; } = false;
