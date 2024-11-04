1683b38e983766acb8323dc01b72b0e92b650f1a
Nikolaj Isager Kruuse <nik@ruc.dk>
2024-11-04 15:39:25 +0100

    Failing xUnit tests

--- xUnit_Handin_2/UnitTest1.cs
+++ xUnit_Handin_2/UnitTest1.cs
@@ -1,12 +1,35 @@
+using HandIn_2_Gr_1;
+using Xunit;
+
 namespace xUnit_Handin_2
 {
-    public class UnitTest1
+    public class DataServicePersonTests
     {
         [Fact]
-        public void Test1()
+        public void GetPerson_ValidID_ReturnsPerson()
         {
+            var validId = "nm0000001";
+            var service = new DataServicePerson();
 
+            var result = DataServicePerson.GetPerson(validId);
 
+            Assert.NotNull(result);
+            Assert.Equal(validId, result.Nconst);
+            Assert.NotNull(result.Primaryname);
+            Assert.IsType<string>(result.Primaryname);
+            Assert.IsType<string>(result.Nconst);
+            Assert.IsType<string>(result.Birthyear);
+
+        }
+        [Fact]
+        public void GetPerson_InvalidID_ReturnsNull()
+        {
+            var invalidId = "nm10000000";
+            var service = new DataServicePerson();
+
+            var result = DataServicePerson.GetPerson(invalidId);
+
+            Assert.Null(result);
         }
     }
 }
