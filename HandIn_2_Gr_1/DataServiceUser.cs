16cb002aa5092f0dc98c91f5e1c04d9460f353a7
Benj4865 <bena380dk@gmail.com>
2024-11-04 16:40:56 +0100

    Reverting unittest after mergen screwup

--- HandIn_2_Gr_1/DataServiceUser.cs
+++ HandIn_2_Gr_1/DataServiceUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HandIn_2_Gr_1.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,10 @@ namespace HandIn_2_Gr_1
 {
     public class DataServiceUser
     {
-       
+       public IList<User> UserList()
+        {
+            return null;
+        }
 
     }
 }
