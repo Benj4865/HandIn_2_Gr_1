ef49b46a3973bc470c709044584a05755993c351
Nikolaj Isager Kruuse <nik@ruc.dk>
2024-11-04 18:05:01 +0100

    Commit after completely re-cloning the project because it would not work.

--- xUnit_Handin_2/UnitTest1.cs
+++ xUnit_Handin_2/UnitTest1.cs
@@ -3,8 +3,41 @@ using HandIn_2_Gr_1.Types;
 
 namespace xUnit_Handin_2
 {
-    public class UnitTest1
+    public class DataServicePersonTest
     {
+
+        [Fact]
+        public void GetPerson_ValidID_ReturnsPerson()
+        {
+            var validId = "nm0000001";
+            var service = new DataServicePerson();
+
+            var result = DataServicePerson.GetPerson(validId);
+
+            Assert.NotNull(result);
+            Assert.Equal(validId, result.Nconst);
+            Assert.NotNull(result.Primaryname);
+            Assert.IsType<string>(result.Primaryname);
+            Assert.IsType<string>(result.Nconst);
+            Assert.IsType<string>(result.Birthyear);
+
+        }
+        [Fact]
+        public void GetPerson_InvalidID_ReturnsNull()
+        {
+            var invalidId = "nm10000000";
+            var service = new DataServicePerson();
+
+            var result = DataServicePerson.GetPerson(invalidId);
+
+            Assert.Null(result);
+        }
+        [Fact]
+        public void ()
+        {
+        
+        }
+
         [Fact]
         public void TestIsEpisode()
         {
