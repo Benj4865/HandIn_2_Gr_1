3efc65d0366a9d010206201fe6726f9b62fd3bef
Benj4865 <bena380dk@gmail.com>
2024-11-04 13:17:04 +0100

    Added fucntioning test

--- xUnit_Handin_2/UnitTest1.cs
+++ xUnit_Handin_2/UnitTest1.cs
@@ -11,22 +11,26 @@ namespace xUnit_Handin_2
 
         }
 
-
+        // Tests wether or not the name of a specific person is correct
         [Fact]
         public void CheckNameOnPerson()
         {
             var Service = new DataServicePerson();
             Person person = DataServicePerson.GetPerson("nm11345295");
             Assert.Equal("María Alejandra Mosquera", person.Primaryname);
-
         }
 
+
+        // Tests wether or not a specific episode of a tv-series is returned, and tests the name of it
         [Fact]
         public void CheckEpisodesOnTConst()
-        { 
-
-
-        
+        {
+            var service = new DataServiceTitle();
+            IList<Title> titleList = DataServiceTitle.FindEpisodesFromSeriesTconst("tt0108778");
+            Title episode = titleList[0];
+            Assert.Equal("The One Where Chandler Can't Remember Which Sister", episode.PrimaryTitle);
         }
+
+
     }
 }
