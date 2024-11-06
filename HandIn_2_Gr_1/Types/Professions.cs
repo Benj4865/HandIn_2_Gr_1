548d23ec0ea816c74f963ae27eaa90c77d916683
Nikolaj Isager Kruuse <nik@ruc.dk>
2024-11-06 14:18:43 +0100

    Quick commit

--- HandIn_2_Gr_1/Types/Professions.cs
+++ HandIn_2_Gr_1/Types/Professions.cs
@@ -8,7 +8,7 @@ namespace HandIn_2_Gr_1.Types
 {
     public class Professions
     {
-        public int professionID { get; set; }
+        public int nConst { get; set; } // rettet fre professionsID husk at spørge om det er korrekt!
 
         public string professionName { get; set; }
 
