936c383c9b0d631675a3b199ff17f3a835465fce
Benj4865 <bena380dk@gmail.com>
2025-01-02 08:03:50 +0100

    Last Commit. All Clear

--- HandIn_2_Gr_1/DataServiceTitle.cs
+++ HandIn_2_Gr_1/DataServiceTitle.cs
@@ -11,7 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
-
+//Last Commit.
 namespace HandIn_2_Gr_1
 {
     public class DataServiceTitle : IDataServiceTitle
